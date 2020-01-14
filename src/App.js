3503962b00b1e3eac0db06fce8c39b6ac70ef5a9
Roman-js <liverkuzen1989@gmail.com>
2020-01-14 20:58:08 +0200

    inint commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import TodoListHeader from "./TodoListHeader";
+import TodoListTasks from "./TodoListTasks";
+import TodoListFooter from "./TodoListFooter";
+
+class App extends React.Component {
+
+    tasks = [
+        {title:'CSS' , isDone: true, priority: 'high' },
+        {title: 'JS' , isDone: true, priority: 'low' },
+        {title: 'HTML', isDone: false, priority: 'high'},
+        {title: 'React', isDone: false, priority: 'low'},
+        {title: 'Angular', isDone: true, priority: 'high'},
+    ];
+    filterValue = 'All';
+    render = () => {
+
+        return (
+            <div className="App">
+                <div className="todoList">
+                    <TodoListHeader />
+                    <TodoListTasks tasks={this.tasks} />
+                    <TodoListFooter filterValue={this.filterValue} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
+
---
+++ src/TodoListTasks.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './TodoListTask'
+import TodoListTask from "./TodoListTask";
+
+
+class TodoListTasks extends React.Component   {
+    render = () => {
+
+        let tasksEls = this.props.tasks.map((t) => {
+             return <TodoListTask title={t.title} isDone={t.isDone} priority={t.priority}/>
+        });
+
+
+        return (
+            <div className="todoList-tasks">
+
+                {tasksEls};
+
+                {/* <TodoListTask title={this.props.tasks[0].title} isDone={this.props.tasks[0].isDone}/>
+                <TodoListTask title={this.props.tasks[1].title} isDone={this.props.tasks[1].isDone}/>
+                <TodoListTask title={this.props.tasks[2].title} isDone={this.props.tasks[2].isDone}/>
+                <TodoListTask title={this.props.tasks[3].title} isDone={this.props.tasks[3].isDone}/>
+                */}
+
+            </div>
+        );
+    }
+}
+
+export default TodoListTasks;
+
